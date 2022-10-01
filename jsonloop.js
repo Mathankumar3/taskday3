8966801228e178f2107e09846a624b4be4fb15db
Mathankumar3 <111418417+Mathankumar3@users.noreply.github.com>
2022-10-01 19:42:50 +0530

    Add files via upload
    kkk

---
+++ jsonloop.js
@@ -0,0 +1,46 @@
+//for in
+
+// var person={
+//     "first_name":"mathankumar",
+//      "last_name": "palanisamy",
+//   "phone":99401310,
+// };
+// for (var property in person) {
+//      console.log(property,":",person[property]);
+//  }
+
+//for off
+
+// var p = {
+//     0: "value1",
+//     "b": "value2",
+//     key: "value3"
+// };
+
+// for (var key of Object.keys(p)) {
+//     console.log(key + " -> " + p[key])
+// }
+
+// for loop
+
+// let student = [{
+//     "id" : "1", 
+//     "name" : "arun",
+//     "department" : "automobile",
+//     "city": "coimbatore",
+//     "phone":113456790,
+// },
+// {
+//     "id" : "2", 
+//     "name" : "mathankumar",
+//     "department" : "mechanical",
+//     "city": "erode",
+//     "phone":123456790,
+// }];
+
+// for(let i = 0; i < student.length; i++) {
+//     let obj = student[i];
+
+//     console.log(obj.id);
+// }
+
---
+++ jsonresume.js
@@ -0,0 +1,41 @@
+[{
+    "basics": {
+        "name": "mathankumar",
+        "email":"mathantek@gmail.com",
+        "phone":"1234567901",
+        "gender":"male",
+
+        "address":{
+            "country":"india",
+            "street name":4/90,
+            "villege":"uthandiyur",
+            "post":"uthandiyur",
+            "city":"sathyamangalam",
+        },
+        "education":[
+            {
+                "institute":"university",
+                "area":"mechanical",
+                "degree":"BE",
+                "start-date":"2022-1-1",
+                "end-date":"2024-1-2",
+            }
+        ],
+        "project":[{
+            "name":"app devepment",
+            "start-date":2022-2-3,
+            "end-date":2024-3-4,
+        }
+
+        ],
+        "skills":[
+            {
+                "language":"tamil,english,telugu,hindi",
+                "level":"native speaker",
+                "software":"javascript",
+            }
+        ],
+        
+       
+        },
+ },   ]
